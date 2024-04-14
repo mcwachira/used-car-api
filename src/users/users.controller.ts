f92b7177e447213b3c46f1d76ea824567f71de21
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-04-14 11:40:25 +0300

    Chore:Finished creating the user service and user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
+import {CreateUserDto} from "./dto/create-user.dto";
+import {UsersService} from "./users.service";
+import {UpdateUserDto} from "./dto/update-user.dto";
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    constructor(private usersService: UsersService) {
+    }
+
+    @Post('/signup')
+
+        createUser(@Body() body : CreateUserDto){
+
+        const {email, password} = body
+   return this.usersService.create(email, password)
+        }
+
+
+        @Get()
+        findAllUsers(@Query('email') email:string){
+        return this.usersService.find(email)
+        }
+
+        @Get('/:id')
+    findUser(@Param('id') id:string){
+        return this.usersService.findOne(parseInt(id))
+        }
+
+
+        @Patch('/:id')
+        updateUser(@Param('id') id:string, @Body() body : UpdateUserDto) {
+
+        // const {email, password} = body
+        return this.usersService.update(parseInt(id), body)
+
+
+
+        }
+
+        @Delete('/:id')
+    removeUser(@Param('id') id:string){
+        return this.usersService.remove(parseInt(id))
+        }
+
+}
